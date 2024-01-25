58d610f0ad9057abd1c70a19b32cc67070fa21ea
udeilton de sousa <deilton_black@hotmail.com>
2024-01-25 16:51:10 -0300

    bee1003

---
+++ 1003/index.js
@@ -0,0 +1,12 @@
+var input = require("fs").readFileSync("./stdin", ("utf-8"));
+
+var valores = input.split("\n");
+
+var A = parseInt(valores.shift());
+var B = parseInt(valores.shift());
+
+var soma = A + B;
+
+console.log("SOMA = " + soma);
+
+
